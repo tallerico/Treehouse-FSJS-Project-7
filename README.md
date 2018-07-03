eb6dd5ef85fd096488427a6bf608e1cf5009ea0c
Mike Tallerico <mtallerico1@gmail.com>
2018-07-03 17:38:00 -0400

    grabs data from twitter api

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Treehouse-FSJS-Project-7
+
+
+*To set up your config file
+
+Create config.js file in js folder
+
+In config file create an object literal 
+
+    exports.auth = {
+      consumer_key:         '...',
+      consumer_secret:      '...',
+      access_token:         '...',
+      access_token_secret:  '...'
+    }
+
+
+Make sure in your app.js file that you include config file
+
+    const { auth } = require('./js/config.js');
--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const Twit = require('twit');
+const { auth } = require('./js/config.js');
 const app = express();
+const T = new Twit({
+  consumer_key:         auth.consumer_key,
+  consumer_secret:      auth.consumer_secret,
+  access_token:         auth.access_token,
+  access_token_secret:  auth.access_token_secret
+})
+
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
@@ -9,11 +18,25 @@ app.use('/static', express.static('public'));
 
 app.set('view engine', 'pug');
 
+
+
+
 app.get('/', (req, res) => {
-  res.render('index');
+  T.get('statuses/user_timeline', { screen_name: 'mtallerico1', count: 6 }, (err, data, response) => {
+    const tweetData = data;
+    res.render('index', {tweetData});
+  });
+});
+
+app.get('/json', (req, res) => {
+  T.get('statuses/user_timeline', { screen_name: 'mtallerico1', count: 6 }, (err, data, response) => {
+    const tweetData = data;
+    res.send(tweetData);
+  });
 });
 
 
+
 app.listen(3000, () => {
-  console.log('The application is running on local host port 3000.')
+  console.log('App is running on port 3000.')
 });
